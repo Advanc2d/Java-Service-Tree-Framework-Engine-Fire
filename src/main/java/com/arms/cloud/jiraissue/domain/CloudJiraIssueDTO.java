392b0b34876388d09689038d6ae4d02fed05cf50
ssoohyun <chltngus129@gmail.com>
2023-07-29 18:04:09 +0900

    refactor: [JIRA-2] #comment 이슈 생성 DTO 공통 필드로 수정 - 23.07.29 #close #time 1h +review SR @313devops
    [ Backend-Core::Java-Service-Tree-Framework(JSTF)::ssoohyun ]
    [Requirement Manage] http://www.a-rms.net
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CICD-Deploy] http://www.313.co.kr/jenkins
    [BuildManager] http://www.313.co.kr/spinnaker
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker Hub] https://hub.docker.com/u/313devgrp
    
    [Kubernetes] http://www.313.co.kr:31380
    [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    [DB Admin] http://www.313.co.kr/phpmyadmin/
    [S3 Admin] http://www.313.co.kr/minio/login
    [MEM Admin] http://www.313.co.kr/redis/
    [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices
    [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true
    [Kibana] http://www.313.co.kr/kibana/app/home#/
    [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty
    [ZipKin] http://www.313.co.kr/zipkin/
    [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    [Grafana] http://www.313.co.kr/grafana
    
    [NAS] http://www.313.co.kr:5000/webman/index.cgi
    [Mail] http://www.313.co.kr/mail/
    [Auth] http://www.313.co.kr/auth/
    [RDP] http://www.313.co.kr/guacamole/#/

--- src/main/java/com/arms/cloud/jiraissue/domain/CloudJiraIssueDTO.java
+++ src/main/java/com/arms/cloud/jiraissue/domain/CloudJiraIssueDTO.java
@@ -1,8 +1,5 @@
 package com.arms.cloud.jiraissue.domain;
 
-import java.util.List;
-import java.util.Map;
-
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 import lombok.*;
@@ -17,39 +14,8 @@ import lombok.*;
 public class CloudJiraIssueDTO {
     private String expand;
     private String id;
-    private String self;
     private String key;
+    private String self;
 
     private FieldsDTO fields;
-
-    private List<Watchers> watchers;
-
-    // 수현이가 한 부분에서 수정
-    // private ProjectDTO project;
-    // private IssueTypeDTO issuetype;
-    // private String summary;
-    // private String description;
-
-    @Getter
-    @Setter
-    @Builder
-    @ToString
-    @NoArgsConstructor
-    @AllArgsConstructor
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    public static class Watchers {
-        private Integer status;
-        private WarningCollection warningCollection;
-    }
-
-    @Getter
-    @Setter
-    @Builder
-    @ToString
-    @NoArgsConstructor
-    @AllArgsConstructor
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    public static class WarningCollection {
-        private List<String> warnings;
-    }
 }
