6280fad79222d42525685be0698f1d5ca4d1d77b
Advanc2d <phm940916@gmail.com>
2023-07-29 17:49:37 +0900

    feat: [JAVA-1] #comment 요구사항 IssueType이 없는 IssueTypeScheme에 추가하도록 하는 컨트롤러 및 기능 추가 - 23.07.29 #close #time 1h +review SR @313devops
    [ Backend-Core::Java-Service-Tree-Framework(JSTF)::Advanc2d ]
    [Requirement Manage] http://www.a-rms.net
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CICD-Deploy] http://www.313.co.kr/jenkins
    [BuildManager] http://www.313.co.kr/spinnaker
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker Hub] https://hub.docker.com/u/313devgrp
    
    [Kubernetes] http://www.313.co.kr:31380
    [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    [DB Admin] http://www.313.co.kr/phpmyadmin/
    [S3 Admin] http://www.313.co.kr/minio/login
    [MEM Admin] http://www.313.co.kr/redis/
    [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices
    [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true
    [Kibana] http://www.313.co.kr/kibana/app/home#/
    [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty
    [ZipKin] http://www.313.co.kr/zipkin/
    [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    [Grafana] http://www.313.co.kr/grafana
    
    [NAS] http://www.313.co.kr:5000/webman/index.cgi
    [Mail] http://www.313.co.kr/mail/
    [Auth] http://www.313.co.kr/auth/
    [RDP] http://www.313.co.kr/guacamole/#/

---
+++ src/main/java/com/arms/cloud/jiraissuetypescheme/service/CloudJiraIssueTypeSchemeImpl.java
@@ -0,0 +1,121 @@
+package com.arms.cloud.jiraissuetypescheme.service;
+
+import com.arms.cloud.jiraissuetypescheme.domain.CloudJiraIssueTypeSchemeMappingDTO;
+import com.arms.cloud.jiraissuetypescheme.domain.CloudJiraIssueTypeSchemeMappingValueDTO;
+import com.arms.cloud.jiraissuetypescheme.domain.IssueTypeIdsDTO;
+import com.arms.config.CloudJiraConfig;
+import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+import reactor.core.publisher.Mono;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@AllArgsConstructor
+@Service("cloudJiraIssueTypeScheme")
+public class CloudJiraIssueTypeSchemeImpl implements CloudJiraIssueTypeScheme {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    @Qualifier("cloudJiraConfig")
+    private CloudJiraConfig cloudJiraConfig;
+
+    public CloudJiraIssueTypeSchemeMappingDTO getIssueTypeSchemeMapping() {
+        final WebClient jiraWebClient = cloudJiraConfig.getJiraWebClient();
+        String uri = "/rest/api/3/issuetypescheme/mapping";
+
+        CloudJiraIssueTypeSchemeMappingDTO issueTypeSchemeMapping = jiraWebClient.get()
+                .uri(uri)
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<CloudJiraIssueTypeSchemeMappingDTO>() {}).block();
+
+        return issueTypeSchemeMapping;
+    }
+
+    public String addIssueTypeSchemeReqIssueType() {
+        final WebClient jiraWebClient = cloudJiraConfig.getJiraWebClient();
+
+        CloudJiraIssueTypeSchemeMappingDTO issueTypeSchemeMapping = getIssueTypeSchemeMapping();
+
+        List<CloudJiraIssueTypeSchemeMappingValueDTO> values = issueTypeSchemeMapping.getValues();
+        Map<String, List<String>> issueTypeMap = getIssueTypeMapping(values);
+
+        // 수정필요
+        // DB에서 요구사항 issueType 정보 가져오기
+        String issueTypeId = "10028";
+
+        for (Map.Entry<String, List<String>> entry : issueTypeMap.entrySet()) {
+            String issueTypeSchemeId = entry.getKey();
+            List<String> issueTypeIds = entry.getValue();
+
+            if (issueTypeIds.contains(issueTypeId)) {
+                System.out.println(issueTypeSchemeId + "에는 원하는 issueTypeId(" + issueTypeId + ")가 존재합니다.");
+            } else {
+                System.out.println(issueTypeSchemeId+ "에는 원하는 issueTypeId(" + issueTypeId + ")가 존재하지 않습니다.");
+                ResponseEntity<?>  result = addIssueTypesToIssueTypeScheme(issueTypeSchemeId, issueTypeId);
+
+                if (result.getStatusCode() == HttpStatus.NO_CONTENT) {
+                    System.out.println("이슈 타입 추가 성공");
+                }
+            }
+        }
+
+        return "success";
+    }
+
+    public ResponseEntity<?> addIssueTypesToIssueTypeScheme(String issueTypeSchemeId, String issueTypeId) {
+        final WebClient jiraWebClient = cloudJiraConfig.getJiraWebClient();
+        String uri = "/rest/api/3/issuetypescheme/"+issueTypeSchemeId+"/issuetype";
+        List<String> issueTypeIds = new ArrayList<String>();
+        issueTypeIds.add(issueTypeId);
+
+        IssueTypeIdsDTO dto = new IssueTypeIdsDTO();
+        dto.setIssueTypeIds(issueTypeIds);
+
+        ResponseEntity<?> addIssueTypeScheme = jiraWebClient.put()
+                .uri(uri)
+                .body(Mono.just(dto), IssueTypeIdsDTO.class)
+                .exchange()
+                .flatMap(clientResponse -> clientResponse.bodyToMono(String.class)
+                        .map(body -> clientResponse.statusCode().is2xxSuccessful() ? ResponseEntity.ok(body) : ResponseEntity.status(clientResponse.statusCode()).build())
+                        .switchIfEmpty(Mono.just(ResponseEntity.status(HttpStatus.NO_CONTENT).build()))
+                        .onErrorResume(WebClientResponseException.class, ex -> Mono.error(new Exception(ex.getMessage()))))
+                .block();
+
+        return addIssueTypeScheme;
+    }
+
+    // issueTypeSchemeId에 따른 issueTypeId 종류를 매핑하는 메서드
+    public static Map<String, List<String>> getIssueTypeMapping(List<CloudJiraIssueTypeSchemeMappingValueDTO> values) {
+        Map<String, List<String>> issueTypeMap = new HashMap<>();
+
+        for (CloudJiraIssueTypeSchemeMappingValueDTO item : values) {
+            String issueTypeSchemeId = item.getIssueTypeSchemeId();
+            String issueTypeId = item.getIssueTypeId();
+
+            if (issueTypeSchemeId != null && issueTypeId != null) {
+                if (issueTypeMap.containsKey(issueTypeSchemeId)) {
+                    issueTypeMap.get(issueTypeSchemeId).add(issueTypeId);
+                } else {
+                    List<String> issueTypeIds = new ArrayList<>();
+                    issueTypeIds.add(issueTypeId);
+                    issueTypeMap.put(issueTypeSchemeId, issueTypeIds);
+                }
+            }
+        }
+
+        return issueTypeMap;
+    }
+
+}
