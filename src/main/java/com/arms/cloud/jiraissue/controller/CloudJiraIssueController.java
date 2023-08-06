dfeb7c647bb282ccdefe230d992469449c6fece9
chanho470 <chanho470@naver.com>
2023-08-06 11:36:11 +0900

    feat: [JIRA-4] #comment 이슈 삭제 처리 서브테스크  to 이슈 변환 불가하여 서브테스크 정보를 바탕으로 새로운 이슈 생성방식 구현  23.08.06 #close #time 1h +review SR @313devops
    [ Backend-Core::Java-Service-Tree-Framework(JSTF)::chanho470 ]
    [Requirement Manage] http://www.a-rms.net
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CICD-Deploy] http://www.313.co.kr/jenkins
    [BuildManager] http://www.313.co.kr/spinnaker
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker Hub] https://hub.docker.com/u/313devgrp
    
    [Kubernetes] http://www.313.co.kr:31380
    [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    [DB Admin] http://www.313.co.kr/phpmyadmin/
    [S3 Admin] http://www.313.co.kr/minio/login
    [MEM Admin] http://www.313.co.kr/redis/
    [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices
    [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true
    [Kibana] http://www.313.co.kr/kibana/app/home#/
    [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty
    [ZipKin] http://www.313.co.kr/zipkin/
    [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    [Grafana] http://www.313.co.kr/grafana
    
    [NAS] http://www.313.co.kr:5000/webman/index.cgi
    [Mail] http://www.313.co.kr/mail/
    [Auth] http://www.313.co.kr/auth/
    [RDP] http://www.313.co.kr/guacamole/#/

--- src/main/java/com/arms/cloud/jiraissue/controller/CloudJiraIssueController.java
+++ src/main/java/com/arms/cloud/jiraissue/controller/CloudJiraIssueController.java
@@ -69,8 +69,9 @@ public class CloudJiraIssueController {
             value = {"/delete/{issueKeyOrId}"},
             method = {RequestMethod.DELETE,RequestMethod.GET}
     )
-    public String deleteDataToaRMS(@PathVariable String issueKeyOrId, ModelMap model,
+    public void deleteDataToaRMS(@PathVariable String issueKeyOrId, ModelMap model,
                                               HttpServletRequest request) throws Exception {
-        return cloudJiraIssue.deleteIssue(issueKeyOrId);
+
+        cloudJiraIssue.deleteIssue(issueKeyOrId);
     }
 }
