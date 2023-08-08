1d7a0a6d2dc064d2dba38c0dd203c191201c7b85
Advanc2d <phm940916@gmail.com>
2023-08-08 23:11:47 +0900

    refactor: [ENGINE-2] #comment OnPremise 요구사항 이슈 타입 유무 조회 API 코드 수정 - 23.08.08
    #close #time 1h +review SR @313devops
    
    [ Backend-Core::Java-Service-Tree-Framework(JSTF)::Advanc2d ]
    [Requirement Manage] http://www.a-rms.net
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CICD-Deploy] http://www.313.co.kr/jenkins
    [BuildManager] http://www.313.co.kr/spinnaker
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker Hub] https://hub.docker.com/u/313devgrp
    
    [Kubernetes] http://www.313.co.kr:31380
    [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    [DB Admin] http://www.313.co.kr/phpmyadmin/
    [S3 Admin] http://www.313.co.kr/minio/login
    [MEM Admin] http://www.313.co.kr/redis/
    [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices
    [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true
    [Kibana] http://www.313.co.kr/kibana/app/home#/
    [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty
    [ZipKin] http://www.313.co.kr/zipkin/
    [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    [Grafana] http://www.313.co.kr/grafana
    
    [NAS] http://www.313.co.kr:5000/webman/index.cgi
    [Mail] http://www.313.co.kr/mail/
    [Auth] http://www.313.co.kr/auth/
    [RDP] http://www.313.co.kr/guacamole/#/

--- src/main/java/com/arms/jira/onpremise/jiraissuetype/service/OnPremiseJiraIssueTypeImpl.java
+++ src/main/java/com/arms/jira/onpremise/jiraissuetype/service/OnPremiseJiraIssueTypeImpl.java
@@ -12,9 +12,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @Service("onPremiseJiraIssueType")
@@ -43,20 +45,16 @@ public class OnPremiseJiraIssueTypeImpl implements OnPremiseJiraIssueType{
     }
 
     public IssueType getIssueTypeListByIssueTypeId(String connectId, String issueTypeId) throws Exception {
-        JiraInfoDTO jiraInfoDTO = jiraInfo.loadConnectInfo(connectId);
-        JiraRestClient restClient = OnPremiseJiraUtils.getJiraRestClient(jiraInfoDTO.getUri(),
-                                                                jiraInfoDTO.getUserId(),
-                                                                jiraInfoDTO.getPasswordOrToken());
+        List<IssueType> issueTypes = getOnPremiseIssueTypeListAll(connectId);
 
-        List<IssueType> issueTypes = (List<IssueType>) restClient.getMetadataClient().getIssueTypes().get();
+        IssueType result = issueTypes.stream()
+                .filter(it -> it.getId() == Integer.parseInt(issueTypeId)) // 주어진 ID와 일치하는 IssueType을 찾습니다.
+                .findFirst().orElse(null); // 결과가 없으면 null을 반환합니다.
 
-        IssueType result = null;
-
-        for (IssueType issueType : issueTypes) {
-            if (issueType.getId() == Integer.parseInt(issueTypeId)) {
-                result =  issueType;
-                break;
-            }
+        if (result != null) {
+            System.out.println("찾은 IssueType: " + result);
+        } else {
+            System.out.println("ID가 " + issueTypeId + "인 IssueType을 찾을 수 없습니다.");
         }
 
         logger.info(result.toString());
@@ -67,29 +65,23 @@ public class OnPremiseJiraIssueTypeImpl implements OnPremiseJiraIssueType{
     public Map<String, Object> checkReqIssueType(String connectId) throws Exception {
         List<IssueType> issueTypes = getOnPremiseIssueTypeListAll(connectId);
 
+        List<String> searchTerms = Arrays.asList("요구사항", "Requirement");
+
+        List<IssueType> issueTypeResult = issueTypes.stream()
+                .filter(it -> searchTerms.contains(it.getName()))
+                .collect(Collectors.toList());
+
         Map<String, Object> result = new HashMap<String, Object>();
 
-        for (IssueType issueType : issueTypes) {
-            if ("요구사항".equals(issueType.getName())) {
-                result.put("success" , true);
-                result.put("message" , "요구사항 이슈 타입이 존재합니다.");
-                result.put("issueTypeId", issueType.getId());
-
-                break;
-            }
-            else if ("requirement".equalsIgnoreCase(issueType.getName())) {
-                result.put("success" , true);
-                result.put("message" , "요구사항 이슈 타입이 존재합니다.");
-                result.put("issueTypeId", issueType.getId());
-
-                break;
-            }
-        }
-
-        if (result.size() == 0) {
+        if (issueTypeResult.isEmpty()) {
             result.put("success" , false);
             result.put("message" , "요구사항 이슈 타입이 존재하지 않습니다.");
             result.put("issueTypeId", null);
+        }
+        else {
+            result.put("success" , true);
+            result.put("message" , "요구사항 이슈 타입이 존재합니다.");
+            result.put("issueTypeId", issueTypeResult.get(0).getId());
         }
 
         return result;
