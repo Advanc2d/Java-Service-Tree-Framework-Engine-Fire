64dfc07cae86f3c124c6da32b98d414461a2df02
ssoohyun <chltngus129@gmail.com>
2023-08-08 17:50:28 +0900

    feat: [JIRA-7] #comment 온프레미스 프로젝트 조회 기능 추가 - 23.08.08 #close #time 1h +review SR @313devops
    [ Backend-Core::Java-Service-Tree-Framework(JSTF)::ssoohyun ]
    [Requirement Manage] http://www.a-rms.net
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CICD-Deploy] http://www.313.co.kr/jenkins
    [BuildManager] http://www.313.co.kr/spinnaker
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker Hub] https://hub.docker.com/u/313devgrp
    
    [Kubernetes] http://www.313.co.kr:31380
    [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    [DB Admin] http://www.313.co.kr/phpmyadmin/
    [S3 Admin] http://www.313.co.kr/minio/login
    [MEM Admin] http://www.313.co.kr/redis/
    [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices
    [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true
    [Kibana] http://www.313.co.kr/kibana/app/home#/
    [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty
    [ZipKin] http://www.313.co.kr/zipkin/
    [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    [Grafana] http://www.313.co.kr/grafana
    
    [NAS] http://www.313.co.kr:5000/webman/index.cgi
    [Mail] http://www.313.co.kr/mail/
    [Auth] http://www.313.co.kr/auth/
    [RDP] http://www.313.co.kr/guacamole/#/

---
+++ src/main/java/com/arms/jira/onpremise/jiraproject/service/OnPremiseOnPremiseJiraProjectImpl.java
@@ -0,0 +1,69 @@
+package com.arms.jira.onpremise.jiraproject.service;
+
+import com.arms.jira.onpremise.jiraproject.model.OnPremiseJiraProjectDTO;
+import com.atlassian.jira.rest.client.api.JiraRestClient;
+import com.atlassian.jira.rest.client.api.domain.BasicProject;
+import com.arms.jira.info.model.JiraInfoDTO;
+import com.arms.jira.info.service.JiraInfo;
+import com.arms.jira.onpremise.JiraUtils;
+import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@Service("jiraProject")
+public class OnPremiseOnPremiseJiraProjectImpl implements OnPremiseJiraProject {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private JiraInfo jiraInfo;
+
+    @Override
+    public List<OnPremiseJiraProjectDTO> getProjectList(String connectId) throws Exception {
+
+        JiraInfoDTO info = jiraInfo.loadConnectInfo(connectId);
+        JiraRestClient restClient = JiraUtils.getJiraRestClient(info.getUri(),
+                                                                info.getUserId(),
+                                                                info.getPasswordOrToken());
+
+        Iterable<BasicProject> allProject = restClient.getProjectClient().getAllProjects().claim();
+        List<OnPremiseJiraProjectDTO> projectList = new ArrayList<>();
+
+        for (BasicProject project : allProject) {
+
+            OnPremiseJiraProjectDTO jiraProject = new OnPremiseJiraProjectDTO();
+            jiraProject.setSelf(project.getSelf().toString());
+            jiraProject.setId(project.getId().toString());
+            jiraProject.setKey(project.getKey());
+            jiraProject.setName(project.getName());
+
+            projectList.add(jiraProject);
+        }
+
+        return projectList;
+    }
+
+    @Override
+    public OnPremiseJiraProjectDTO getProject(String connectId, String projectKey) throws Exception {
+        JiraInfoDTO info = jiraInfo.loadConnectInfo(connectId);
+        JiraRestClient restClient = JiraUtils.getJiraRestClient(info.getUri(),
+                                                                info.getUserId(),
+                                                                info.getPasswordOrToken());
+
+        BasicProject project = restClient.getProjectClient().getProject(projectKey).claim();
+
+        OnPremiseJiraProjectDTO onPremiseJiraProjectDTO = new OnPremiseJiraProjectDTO();
+        onPremiseJiraProjectDTO.setSelf(project.getSelf().toString());
+        onPremiseJiraProjectDTO.setId(project.getId().toString());
+        onPremiseJiraProjectDTO.setKey(project.getKey());
+        onPremiseJiraProjectDTO.setName(project.getName());
+
+        return onPremiseJiraProjectDTO;
+    }
+
+}
