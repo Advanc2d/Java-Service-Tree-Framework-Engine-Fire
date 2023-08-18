9a5bd1e565e0f09af037f9b2e592f5fa061c4239
Advanc2d <phm940916@gmail.com>
2023-08-19 00:44:00 +0900

    test: [ENGINE-5] #comment Issue Subtask, Inward Link 스케줄러 Test Code - 23.08.18
    1. Cloud Issue Subtask, Inward Link 조회
    2. OnPremise Issue Subtask, Inward Link 조회
    
    #close #time 1h +review SR @313devops
    
    [ Backend-Core::Java-Service-Tree-Framework(JSTF)::Advanc2d ]
    [Requirement Manage] http://www.a-rms.net
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CICD-Deploy] http://www.313.co.kr/jenkins
    [BuildManager] http://www.313.co.kr/spinnaker
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker Hub] https://hub.docker.com/u/313devgrp
    
    [Kubernetes] http://www.313.co.kr:31380
    [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    [DB Admin] http://www.313.co.kr/phpmyadmin/
    [S3 Admin] http://www.313.co.kr/minio/login
    [MEM Admin] http://www.313.co.kr/redis/
    [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices
    [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true
    [Kibana] http://www.313.co.kr/kibana/app/home#/
    [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty
    [ZipKin] http://www.313.co.kr/zipkin/
    [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    [Grafana] http://www.313.co.kr/grafana
    
    [NAS] http://www.313.co.kr:5000/webman/index.cgi
    [Mail] http://www.313.co.kr/mail/
    [Auth] http://www.313.co.kr/auth/
    [RDP] http://www.313.co.kr/guacamole/#/

---
+++ src/test/java/com/engine/jira/onpremise/scheduler/OnPremiseIssueSchedulerTest.java
@@ -0,0 +1,171 @@
+package com.engine.jira.onpremise.scheduler;
+
+import com.arms.jira.cloud.CloudJiraUtils;
+import com.arms.jira.onpremise.jiraissue.model.FieldsDTO;
+import com.arms.jira.onpremise.jiraissue.model.OnPremiseJiraIssueDTO;
+import com.arms.jira.onpremise.jiraissue.model.OnPremiseJiraIssueEntity;
+import com.atlassian.jira.rest.client.api.JiraRestClient;
+import com.atlassian.jira.rest.client.api.domain.Issue;
+import com.atlassian.jira.rest.client.api.domain.SearchResult;
+import com.atlassian.jira.rest.client.internal.async.AsynchronousJiraRestClientFactory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.*;
+
+public class OnPremiseIssueSchedulerTest {
+    JiraRestClient restClient;
+    public static ModelMapper modelMapper = new ModelMapper();
+    public String baseUrl = "http://www.313.co.kr/jira";
+    public String id = "admin";
+    public String pass = "flexjava";
+    public String projectKeyOrId = "ARMS";
+    public String issueKeyOrId = projectKeyOrId + "-1";
+
+    @BeforeEach
+    void setUp () throws URISyntaxException, IOException {
+        restClient = getJiraRestClient(baseUrl, id, pass);
+    }
+
+    public static JiraRestClient getJiraRestClient(String jiraUrl, String jiraID, String jiraPass) throws URISyntaxException, IOException {
+        final AsynchronousJiraRestClientFactory factory = new AsynchronousJiraRestClientFactory();
+        return factory.createWithBasicHttpAuthentication(new URI(jiraUrl), jiraID, jiraPass);
+    }
+
+    @Test
+    @DisplayName("이슈 타입이 요구사항인 이슈를 전체 조회하고 이슈 링크 내용을 전부 가져오는 스케줄러")
+    public void test() throws Exception {
+        SearchResult searchResult = getIssueListByIssueTypeName("Requirement");
+        List<OnPremiseJiraIssueDTO> issues = new ArrayList<>();
+        Iterable<Issue> issues1 = searchResult.getIssues();
+        for (Issue issue1 : issues1) {
+            if(issue1.getProject().getKey().equals("PHM")) {
+                OnPremiseJiraIssueDTO onPremiseJiraIssueDTO = new OnPremiseJiraIssueDTO();
+                onPremiseJiraIssueDTO.setId(issue1.getId().toString());
+                onPremiseJiraIssueDTO.setKey(issue1.getKey());
+                onPremiseJiraIssueDTO.setSelf(issue1.getSelf().toString());
+
+                issues.add(onPremiseJiraIssueDTO);
+            }
+        }
+
+        List<OnPremiseJiraIssueEntity> allDtos = new ArrayList<>();
+        try {
+            for (OnPremiseJiraIssueDTO issue : issues) {
+                List<OnPremiseJiraIssueEntity> issueLinkDTOs = findAllLinkedDtos(issue, new ArrayList<>(), null, null);
+
+                allDtos.addAll(issueLinkDTOs);
+                // printLinkedIssues(issueLinkDTO, 0);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("allDtos = " + allDtos.size());
+        for (OnPremiseJiraIssueEntity dto : allDtos) {
+            System.out.println(dto.getKey() +"/"+ dto.getOutwardId() +"/"+ dto.getParentId());
+        }
+    }
+
+    public List<OnPremiseJiraIssueEntity> findAllLinkedDtos(OnPremiseJiraIssueDTO dto, List<OnPremiseJiraIssueEntity> allDtos,
+                                                            String outwardId, String parentId) throws Exception {
+        // 현재 DTO의 하위에 연결된 DTO들을 allDtos에 추가
+        OnPremiseJiraIssueEntity entity = modelMapper.map(dto, OnPremiseJiraIssueEntity.class);
+
+        if (outwardId != null) {
+            entity.setOutwardId(outwardId);
+        }
+
+        if(parentId != null) {
+            entity.setParentId(parentId);
+        }
+
+        allDtos.add(entity);
+
+        System.out.println("dto.getKey() = " + dto.getKey());
+        dto = getIssueByWebClient(dto.getKey());
+
+        // 현재 DTO와 연결된 모든 하위 DTO를 탐색
+        if (dto.getFields().getIssuelinks() != null) {
+            for (FieldsDTO.IssueLink issueLink : dto.getFields().getIssuelinks()) {
+                if (issueLink.getInwardIssue() == null) {
+                    continue;
+                }
+
+                System.out.println("issueLink.getInwardIssue().getKey() = " + issueLink.getInwardIssue().getKey());
+                OnPremiseJiraIssueDTO onPremiseJiraIssueDTO = getIssueByWebClient(issueLink.getInwardIssue().getKey());
+
+                findAllLinkedDtos(onPremiseJiraIssueDTO, allDtos, dto.getId(), null);
+            }
+        }
+
+        if (dto.getFields().getSubtasks() != null ) {
+            for (OnPremiseJiraIssueDTO subtask : dto.getFields().getSubtasks()) {
+                System.out.println("subtask.getKey() = " + subtask.getKey());
+                OnPremiseJiraIssueDTO subtaskDTO = getIssueByWebClient(subtask.getKey());
+                findAllLinkedDtos(subtaskDTO, allDtos, null, dto.getId());
+            }
+        }
+
+        return allDtos;
+    }
+
+    public Issue getIssue(String issueKeyOrId) throws Exception {
+
+        Issue issue = restClient.getIssueClient().getIssue(issueKeyOrId).claim();
+
+        return issue;
+    }
+
+    public SearchResult getIssueListByIssueTypeName(String issueTypeName) throws Exception {
+
+        String jql = "issuetype = " + issueTypeName;
+        int maxResults = 1000;
+        int startAt = 0;
+        Set<String> fields = new HashSet<>(Arrays.asList("*all")); // 검색 필드
+
+        // 이슈 건수가 1000이 넘을때 이슈 조회를 위한 처리
+        List<Issue> allIssues = new ArrayList<>();
+        SearchResult searchResult;
+
+        do {
+            searchResult = restClient.getSearchClient()
+                    .searchJql(jql, maxResults, startAt, fields)
+                    .get();
+            for (Issue issue : searchResult.getIssues()) {
+                allIssues.add(issue);
+            }
+            startAt += maxResults;
+        } while (searchResult.getTotal() > startAt);
+
+        return searchResult;
+    }
+
+    public OnPremiseJiraIssueDTO getIssueByWebClient(String issueKeyOrId) throws Exception {
+
+        String endpoint = "/rest/api/2/issue/" + issueKeyOrId;
+        WebClient webClient = createJiraWebClient(baseUrl);
+
+        OnPremiseJiraIssueDTO onPremiseJiraIssueDTO = CloudJiraUtils.get(webClient, endpoint, OnPremiseJiraIssueDTO.class).block();
+
+        return onPremiseJiraIssueDTO;
+    }
+
+    public static WebClient createJiraWebClient(String uri) {
+
+        return WebClient.builder()
+                .baseUrl(uri)
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+                .build();
+    }
+}
