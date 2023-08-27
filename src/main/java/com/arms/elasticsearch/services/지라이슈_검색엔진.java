8ad8706dc19d43031c20d96976acab43712cd85d
dmlee-mz <dmlee@mz.co.kr>
2023-08-27 14:24:09 +0900

    feat: [ARMSQA-19] #comment 검색엔진 조회 코드 오류 픽스 9차 - 유틸라이제이션으로 변경 - 23.08.26 #close #time 1h +review SR @313devops
    [ Engine-Fire::Java-Service-Tree-Framework(JSTF)::313DEVGRP ]
    요구 관리 : [Requirement Manage] http://www.a-rms.net
    문서 관리 : [Document] http://www.313.co.kr/confluence
    이슈 관리 : [IssueTracker] http://www.313.co.kr/jira
    소스 관리 : [VersionControl] https://github.com/313devgrp
    코드 리뷰 : [CodeReview] http://www.313.co.kr/fecru
    빌드 관리 : [CICD-Deploy] http://www.313.co.kr/jenkins
    배포 관리 : [Deploy-Pipeline] http://www.313.co.kr/spinnaker
    아티 팩트 : [ArtifactManager] http://www.313.co.kr/nexus
    코드 분석 : [CodeAnalysis] http://www.313.co.kr/sonar
    성능 측정 : [ZipKin] http://www.313.co.kr/zipkin/
    
    도커 관리 : [Docker Hub] https://hub.docker.com/u/313devgrp
    클러스터1 : [Kubernetes] http://www.313.co.kr:31380
    클러스터2 : [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    디비 관리 : [DB Admin] http://www.313.co.kr/phpmyadmin/
    오브 젝트 : [S3 Admin] http://www.313.co.kr/minio/login
    캐시 관리 : [Cache Admin] http://www.313.co.kr/redis/
    ELFK 스택1 : [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices
    ELFK 스택2 : [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true
    ELFK 스택3 : [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty
    ELFK 스택4 : [Kibana] http://www.313.co.kr/kibana/app/home#/
    
    모니터링 1 : [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    모니터링 2 : [Grafana] http://www.313.co.kr/grafana
    
    인증 인가 : [Auth] http://www.313.co.kr/auth/
    
    NFS 관리 : [NAS] http://www.313.co.kr:5000/webman/index.cgi
    메일 관리 : [Mail] http://www.313.co.kr/mail/
    원격 관리 : [RDP] http://www.313.co.kr/guacamole/#/

--- src/main/java/com/arms/elasticsearch/services/지라이슈_검색엔진.java
+++ src/main/java/com/arms/elasticsearch/services/지라이슈_검색엔진.java
@@ -30,17 +30,16 @@ import java.util.List;
 @Service("지라이슈_서비스")
 public class 지라이슈_검색엔진 implements 지라이슈_서비스{
 
-    private static final ObjectMapper MAPPER = new ObjectMapper();
-
     private 지라이슈_저장소 지라이슈저장소;
 
-    private final RestHighLevelClient client;
+    private 검색유틸 검색유틸;
+
 
     @Autowired
-    public 지라이슈_검색엔진(final 지라이슈_저장소 지라이슈저장소, RestHighLevelClient client) {
+    public 지라이슈_검색엔진(final 지라이슈_저장소 지라이슈저장소, final 검색유틸 검색유틸) {
         super();
         this.지라이슈저장소 = 지라이슈저장소;
-        this.client = client;
+        this.검색유틸 = 검색유틸;
     }
 
 
@@ -86,88 +85,9 @@ public class 지라이슈_검색엔진 implements 지라이슈_서비스{
                 검색조건
         );
 
-        return searchInternal(request);
+        return 검색유틸.searchInternal(request);
     }
 
-    private List<지라이슈> searchInternal(final SearchRequest request) {
-        if (request == null) {
-            log.error("Failed to build search request");
-            return Collections.emptyList();
-        }
-
-        try {
-            final SearchResponse response = client.search(request, RequestOptions.DEFAULT);
-
-            final SearchHit[] searchHits = response.getHits().getHits();
-            final List<지라이슈> vehicles = new ArrayList<>(searchHits.length);
-
-            for (SearchHit hit : searchHits) {
-
-                vehicles.add(
-                        MAPPER.readValue(hit.getSourceAsString(), 지라이슈.class)
-                );
-            }
-
-            return vehicles;
-        } catch (Exception e) {
-            log.error(e.getMessage(), e);
-            return Collections.emptyList();
-        }
-    }
-
-    public List<지라이슈> getAllCreatedSince(final Date date) {
-        final SearchRequest request = 검색유틸.buildSearchRequest(
-                인덱스자료.지라이슈_인덱스명,
-                "created",
-                date
-        );
-
-        return searchInternal(request);
-    }
-
-    public List<지라이슈> searchCreatedSince(final SearchDTO dto, final Date date) {
-        final SearchRequest request = 검색유틸.buildSearchRequest(
-                인덱스자료.지라이슈_인덱스명,
-                dto,
-                date
-        );
-
-        return searchInternal(request);
-    }
-
-    public Boolean index(final 지라이슈 지라_이슈) {
-        try {
-            final String vehicleAsString = MAPPER.writeValueAsString(지라_이슈);
-
-            final IndexRequest request = new IndexRequest(인덱스자료.지라이슈_인덱스명);
-            request.id(지라_이슈.getId());
-            request.source(vehicleAsString, XContentType.JSON);
-
-            final IndexResponse response = client.index(request, RequestOptions.DEFAULT);
-
-            return response != null && response.status().equals(RestStatus.OK);
-        } catch (final Exception e) {
-            log.error(e.getMessage(), e);
-            return false;
-        }
-    }
-
-    public 지라이슈 getById(final String 이슈_아이디) {
-        try {
-            final GetResponse documentFields = client.get(
-                    new GetRequest(인덱스자료.지라이슈_인덱스명, 이슈_아이디),
-                    RequestOptions.DEFAULT
-            );
-            if (documentFields == null || documentFields.isSourceEmpty()) {
-                return null;
-            }
-
-            return MAPPER.readValue(documentFields.getSourceAsString(), 지라이슈.class);
-        } catch (final Exception e) {
-            log.error(e.getMessage(), e);
-            return null;
-        }
-    }
 
 
 }
