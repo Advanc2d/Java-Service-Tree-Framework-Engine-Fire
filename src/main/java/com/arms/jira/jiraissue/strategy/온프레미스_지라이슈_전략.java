3738c99f5c878e41bd2d7a76c9bfd3916d270a43
Advanc2d <phm940916@gmail.com>
2023-09-03 16:01:11 +0900

    :see_no_evil: refactor: [ENGINE] #comment 삭제된 이슈 조회 시 exception처리 제거 - 23.09.03
    서브테스크, 링크드 이슈 조회 중 오류발생 시 null 처리
    
    지라서버에서 삭제된 요구사항 이슈를 조회 시 오류 발생처리에서 null로 처리 후 elk에 저장 시 상태값 변경하여 저장하도록 수정
    
    #close #time 1h +review SR @313devops
    
    [ Backend-Core::Java-Service-Tree-Framework(JSTF)::Advanc2d ]
    [Requirement Manage] http://www.a-rms.net
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CICD-Deploy] http://www.313.co.kr/jenkins
    [BuildManager] http://www.313.co.kr/spinnaker
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker Hub] https://hub.docker.com/u/313devgrp
    
    [Kubernetes] http://www.313.co.kr:31380
    [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    [DB Admin] http://www.313.co.kr/phpmyadmin/
    [S3 Admin] http://www.313.co.kr/minio/login
    [MEM Admin] http://www.313.co.kr/redis/
    [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices
    [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true
    [Kibana] http://www.313.co.kr/kibana/app/home#/
    [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty
    [ZipKin] http://www.313.co.kr/zipkin/
    [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    [Grafana] http://www.313.co.kr/grafana
    
    [NAS] http://www.313.co.kr:5000/webman/index.cgi
    [Mail] http://www.313.co.kr/mail/
    [Auth] http://www.313.co.kr/auth/
    [RDP] http://www.313.co.kr/guacamole/#/

--- src/main/java/com/arms/jira/jiraissue/strategy/온프레미스_지라이슈_전략.java
+++ src/main/java/com/arms/jira/jiraissue/strategy/온프레미스_지라이슈_전략.java
@@ -103,7 +103,7 @@ public class 온프레미스_지라이슈_전략 implements 지라이슈_전략 
 
         }catch (Exception e){
             로그.error("온프레미스 이슈 조회시 오류가 발생하였습니다.");
-            throw new IllegalArgumentException(에러코드.이슈_조회_오류.getErrorMsg());
+            return null;
         }
     }
 
@@ -499,230 +499,6 @@ public class 온프레미스_지라이슈_전략 implements 지라이슈_전략 
         return 포맷팅.toString().trim();
     }
 
-    /*
-    private 지라이슈_데이터 지라이슈_데이터로_변환(Issue 지라이슈) {
-
-        지라이슈_데이터 반환할_지라_이슈_데이터= new 지라이슈_데이터();
-        지라이슈필드_데이터 지라이슈필드_데이터 = new 지라이슈필드_데이터();
-
-        // 프로젝트 초기화
-        지라프로젝트_데이터 프로젝트 = 지라프로젝트_데이터.builder().build();
-
-        // 보고자 초기화
-        지라사용자_데이터 보고자 = 지라사용자_데이터.builder().build();
-
-        // 담당자 초기화
-        지라사용자_데이터 담당자 = 지라사용자_데이터.builder().build();
-
-        // 연결된 이슈 초기화
-//        지라이슈_데이터 내부_연결_이슈 = new 지라이슈_데이터();
-//        지라이슈_데이터 외부_연결_이슈 = new 지라이슈_데이터();
-//
-//        지라_이슈_필드_데이터_전송_객체.연결된_이슈 연결된_이슈 = new 지라_이슈_필드_데이터_전송_객체.연결된_이슈();
-//        연결된_이슈.setInwardIssue(내부_연결_이슈);
-//        연결된_이슈.setOutwardIssue(외부_연결_이슈);
-//       List<지라_이슈_필드_데이터_전송_객체.연결된_이슈> 연결된_이슈_목록 = new ArrayList<>();
-
-        지라이슈필드_데이터.setProject(프로젝트);
-        지라이슈필드_데이터.setReporter(보고자);
-        지라이슈필드_데이터.setAssignee(담당자);
-        // 지라_이슈_필드_데이터.setIssuelinks(연결된_이슈_목록);
-
-
-        반환할_지라_이슈_데이터.setFields(지라이슈필드_데이터);
-
-        반환할_지라_이슈_데이터.setId(지라이슈.getId().toString());
-        반환할_지라_이슈_데이터.setKey(지라이슈.getKey());
-        반환할_지라_이슈_데이터.setSelf(지라이슈.getSelf().toString());
-
-        // 필드 하위 프로젝트
-        반환할_지라_이슈_데이터.getFields().getProject().setSelf(지라이슈.getProject().getSelf().toString());
-        반환할_지라_이슈_데이터.getFields().getProject().setId(String.valueOf(지라이슈.getProject().getId()));
-        반환할_지라_이슈_데이터.getFields().getProject().setKey(지라이슈.getProject().getKey());
-        반환할_지라_이슈_데이터.getFields().getProject().setName(지라이슈.getProject().getName());
-
-        // 필드 하위 이슈 타입   지라_이슈_유형_데이터_전송_객체
-        if(지라이슈.getIssueType()!= null){
-            지라이슈유형_데이터 이슈_유형 =new 지라이슈유형_데이터();
-            지라이슈필드_데이터.setIssuetype(이슈_유형);
-            반환할_지라_이슈_데이터.setFields(지라이슈필드_데이터);
-
-            String 이슈_유형_주소 = String.valueOf(지라이슈.getIssueType().getSelf());
-            String 이슈_유형_아이디 = String.valueOf(지라이슈.getIssueType().getId());
-            String 이슈_유형_이름 =지라이슈.getIssueType().getName();
-
-            반환할_지라_이슈_데이터.getFields().getIssuetype().setId(이슈_유형_아이디);
-            반환할_지라_이슈_데이터.getFields().getIssuetype().setName(이슈_유형_이름);
-            반환할_지라_이슈_데이터.getFields().getIssuetype().setSelf(이슈_유형_주소);
-        }
-
-        // 이슈 summary
-        // 반환할_지라_이슈_데이터.getFields().setSummary(지라이슈.getSummary());
-
-        // 이슈 description
-        // 반환할_지라_이슈_데이터.getFields().setDescription(지라이슈.getDescription());
-
-        // 이슈 보고자
-        // 반환할_지라_이슈_데이터.getFields().getReporter().setName(지라이슈.getReporter().getName());
-        반환할_지라_이슈_데이터.getFields().getReporter().setAccountId(지라이슈.getReporter().getName());
-        반환할_지라_이슈_데이터.getFields().getReporter().setEmailAddress(지라이슈.getReporter().getEmailAddress());
-
-        // 이슈 담당자
-        // 반환할_지라_이슈_데이터.getFields().getAssignee().setName(지라이슈.getAssignee().getName());
-        반환할_지라_이슈_데이터.getFields().getAssignee().setAccountId(지라이슈.getAssignee().getName());
-        반환할_지라_이슈_데이터.getFields().getAssignee().setEmailAddress(지라이슈.getAssignee().getEmailAddress());
-
-        // 이슈 라벨
-        Set<String> 지라_라벨 = 지라이슈.getLabels(); //HashSet 반환
-        if (지라_라벨 != null) {
-            List<String> 라벨_목록 = new ArrayList<>(지라_라벨);
-            반환할_지라_이슈_데이터.getFields().setLabels(라벨_목록);
-        } else {
-            반환할_지라_이슈_데이터.getFields().setLabels(Collections.emptyList());
-        }
-
-
-        // 이슈 링크
-//        List<IssueLink> 연결된_이슈_리스트= new ArrayList<>((Collection) 지라이슈.getIssueLinks());
-//        for (IssueLink 연결된_이슈_항목: 연결된_이슈_리스트) {
-//
-//            String direction = String.valueOf(연결된_이슈_항목.getIssueLinkType().getDirection());
-//            String targetIssueKey = 연결된_이슈_항목.getTargetIssueKey();
-//            String self = String.valueOf(연결된_이슈_항목.getTargetIssueUri());
-//            String[] parts = self.split("/");
-//            String id = parts[parts.length - 1];
-//
-//            지라_이슈_필드_데이터_전송_객체.연결된_이슈 연결된_이슈_필드 = new 지라_이슈_필드_데이터_전송_객체.연결된_이슈();
-//            지라이슈_데이터 내부_연결_이슈_객체 = new 지라이슈_데이터();
-//            지라이슈_데이터 외부_연결_이슈_객체  = new 지라이슈_데이터();
-//
-//            연결된_이슈_필드.setInwardIssue(내부_연결_이슈_객체);
-//            연결된_이슈_필드.setOutwardIssue(외부_연결_이슈_객체);
-//
-//
-//            if(direction.equals("INBOUND")){
-//                //로그.info("direction   "+direction+"   targetIssueKey   "+targetIssueKey);
-//                내부_연결_이슈_객체.setKey(targetIssueKey);
-//                내부_연결_이슈_객체.setSelf(self);
-//                내부_연결_이슈_객체.setId(id);
-//            }
-//            else if(direction.equals("OUTBOUND")){
-//                //로그.info("direction   "+direction+"   targetIssueKey   "+targetIssueKey);
-//                외부_연결_이슈_객체.setKey(targetIssueKey);
-//                외부_연결_이슈_객체.setSelf(self);
-//                외부_연결_이슈_객체.setId(id);
-//            }
-//
-//            반환할_지라_이슈_데이터.getFields().getIssuelinks().add(연결된_이슈_필드);
-//
-//        }
-
-        // 서브 테스크
-//        Iterable<Subtask> 지라_서버_서브테스크_목록 = 지라이슈.getSubtasks();
-//        List<Subtask> 서브테스크_목록 = new ArrayList<>();
-//        for (Subtask 지라_서버_서브테스크 : 지라_서버_서브테스크_목록) {
-//            서브테스크_목록.add(지라_서버_서브테스크);
-//        }
-//        for(Subtask 서브테스크 : 서브테스크_목록){
-//
-//            String 서브테스크_키 = 서브테스크.getIssueKey();
-//            String 서브테스크_주소 = String.valueOf(서브테스크.getIssueUri());
-//            String[] 서브테스크_주소_배열= 서브테스크_주소.split("/");
-//            String 서브테스크_아이디 = 서브테스크_주소_배열[서브테스크_주소_배열.length - 1];
-//
-//            String 서브테스크_이슈타입_주소 = String.valueOf(서브테스크.getIssueType().getSelf());
-//            String[] 서브테스크_이슈타입_주소_배열 = 서브테스크_이슈타입_주소.split("/");
-//            String 서브테스크_이슈타입_아이디 = 서브테스크_이슈타입_주소_배열[서브테스크_이슈타입_주소_배열.length - 1];
-//            String 서브테스크_이슈타입_이름 = 서브테스크.getIssueType().getName();
-//
-//            String 서브테스크_요약 = 서브테스크.getSummary();
-//
-//            String 서브테스크_상태_주소 = String.valueOf(서브테스크.getStatus().getSelf());
-//            String 서브테스크_상태_이름 = 서브테스크.getStatus().getName();
-//            String[] 서브테스크_상태_주소_목록 = 서브테스크_상태_주소.split("/");
-//            String 서브테스크_상태_아이디 = 서브테스크_상태_주소_목록[서브테스크_상태_주소_목록.length - 1];
-//            String 서브테스크_상태_설명 = 서브테스크.getStatus().getDescription();
-//
-//            지라이슈_데이터 서브테스크_객체 = new 지라이슈_데이터();
-//            지라_이슈_필드_데이터_전송_객체 서브테스크_필드_객체 = new 지라_이슈_필드_데이터_전송_객체();
-//
-//            서브테스크_객체.setFields(서브테스크_필드_객체);
-//            서브테스크_객체.getFields().setIssuetype(new 지라_이슈_유형_데이터_전송_객체());
-//            서브테스크_객체.getFields().setPriority(new 지라_이슈_우선순위_데이터_전송_객체());
-//            서브테스크_객체.getFields().setStatus(new 지라_이슈_상태_데이터_전송_객체());
-//
-//            서브테스크_객체.setId(서브테스크_아이디);
-//            서브테스크_객체.setKey(서브테스크_키);
-//            서브테스크_객체.setSelf(서브테스크_주소);
-//
-//            서브테스크_객체.getFields().getIssuetype().setSelf(서브테스크_이슈타입_주소);
-//            서브테스크_객체.getFields().getIssuetype().setId(서브테스크_이슈타입_아이디);
-//            서브테스크_객체.getFields().getIssuetype().setName(서브테스크_이슈타입_이름);
-//
-//            서브테스크_객체.getFields().setSummary(서브테스크_요약);
-//
-//            서브테스크_객체.getFields().getStatus().setId(서브테스크_상태_아이디);
-//            서브테스크_객체.getFields().getStatus().setName(서브테스크_상태_이름);
-//            서브테스크_객체.getFields().getStatus().setDescription(서브테스크_상태_설명);
-//            서브테스크_객체.getFields().getStatus().setSelf(서브테스크_상태_주소);
-//
-//            if (반환할_지라_이슈_데이터.getFields().getSubtasks() == null) {
-//                반환할_지라_이슈_데이터.getFields().setSubtasks(new ArrayList<>());
-//            }
-//            반환할_지라_이슈_데이터.getFields().getSubtasks().add(서브테스크_객체);
-//        }
-
-        // 우선 순위
-        if(지라이슈.getPriority() != null) {
-            지라이슈_우선순위_데이터 이슈_우선순위 = new 지라이슈_우선순위_데이터();
-            지라이슈필드_데이터.setPriority(이슈_우선순위);
-            반환할_지라_이슈_데이터.setFields(지라이슈필드_데이터);
-
-            String 이슈_우선순위_이이디 = String.valueOf(지라이슈.getPriority().getId());
-            String 이슈_우선순위_이름 = 지라이슈.getPriority().getName();
-            String 이슈_우선순위_주소 = String.valueOf(지라이슈.getPriority().getSelf());
-
-            반환할_지라_이슈_데이터.getFields().getPriority().setSelf(이슈_우선순위_주소);
-            반환할_지라_이슈_데이터.getFields().getPriority().setId(이슈_우선순위_이이디);
-            반환할_지라_이슈_데이터.getFields().getPriority().setName(이슈_우선순위_이름);
-        }
-        // 상태 값
-        if(지라이슈.getStatus() != null){
-            지라이슈상태_데이터 이슈_상태 =new 지라이슈상태_데이터();
-            지라이슈필드_데이터.setStatus(이슈_상태);
-            반환할_지라_이슈_데이터.setFields(지라이슈필드_데이터);
-
-            String 이슈_상태_아이디 = String.valueOf(지라이슈.getStatus().getId());
-            String 이슈_상태_이름 = 지라이슈.getStatus().getName();
-            String 이슈_상태_설명 =  지라이슈.getStatus().getDescription();
-            String 이슈_상태_주소 = String.valueOf(지라이슈.getStatus().getSelf());
-
-            반환할_지라_이슈_데이터.getFields().getStatus().setId(이슈_상태_아이디);
-            반환할_지라_이슈_데이터.getFields().getStatus().setName(이슈_상태_이름);
-            반환할_지라_이슈_데이터.getFields().getStatus().setDescription(이슈_상태_설명);
-            반환할_지라_이슈_데이터.getFields().getStatus().setSelf(이슈_상태_주소);
-        }
-        // 해결책
-        if(지라이슈.getResolution()!= null){
-            지라이슈_해결책_데이터 이슈_해결책 =new 지라이슈_해결책_데이터();
-            지라이슈필드_데이터.setResolution(이슈_해결책);
-            반환할_지라_이슈_데이터.setFields(지라이슈필드_데이터);
-
-            String 이슈_해결책_아이디 = String.valueOf(지라이슈.getResolution().getId());
-            String 이슈_해결책_주소 = String.valueOf(지라이슈.getResolution().getSelf());
-            String 이슈_해결책_이름 =지라이슈.getResolution().getName();
-            String 이슈_해결책_설명 = 지라이슈.getResolution().getDescription();
-
-            반환할_지라_이슈_데이터.getFields().getResolution().setDescription(이슈_해결책_설명);
-            반환할_지라_이슈_데이터.getFields().getResolution().setId(이슈_해결책_아이디);
-            반환할_지라_이슈_데이터.getFields().getResolution().setName(이슈_해결책_이름);
-            반환할_지라_이슈_데이터.getFields().getResolution().setSelf(이슈_해결책_주소);
-        }
-
-        return 반환할_지라_이슈_데이터;
-    }
-    */
-
     public List<지라이슈_데이터> 이슈링크_가져오기(Long 연결_아이디, String 이슈_키_또는_아이디) throws URISyntaxException, IOException, ExecutionException, InterruptedException {
 
         로그.info("온프레미스 지라 이슈링크_가져오기");
@@ -742,20 +518,26 @@ public class 온프레미스_지라이슈_전략 implements 지라이슈_전략 
         List<지라이슈_데이터> 반환할_이슈링크_목록 = new ArrayList<>();
         SearchResult searchResult;
 
-        do {
-            searchResult = restClient.getSearchClient()
-                    .searchJql(jql, 최대_검색수, startAt, fields)
-                    .get();
-            for (Issue issue : searchResult.getIssues()) {
-                지라이슈_데이터 지라이슈_데이터 = 지라이슈_데이터로_변환(issue);
-                반환할_이슈링크_목록.add(지라이슈_데이터);
-            }
+        try {
+            do {
+                searchResult = restClient.getSearchClient()
+                        .searchJql(jql, 최대_검색수, startAt, fields)
+                        .get();
+                for (Issue issue : searchResult.getIssues()) {
+                    지라이슈_데이터 지라이슈_데이터 = 지라이슈_데이터로_변환(issue);
+                    반환할_이슈링크_목록.add(지라이슈_데이터);
+                }
 
-            startAt += 최대_검색수;
+                startAt += 최대_검색수;
 
-        } while (searchResult.getTotal() > startAt);
+            } while (searchResult.getTotal() > startAt);
 
-        return 반환할_이슈링크_목록;
+            return 반환할_이슈링크_목록;
+        }
+        catch (Exception e) {
+            로그.error("온프레미스에서 조회하려는 이슈 키의 연결된 이슈 정보 가져오기에 실패하였습니다. 조회 대상 정보 확인이 필요합니다.");
+            return null;
+        }
     }
 
     public List<지라이슈_데이터> 서브테스크_가져오기(Long 연결_아이디, String 이슈_키_또는_아이디) throws URISyntaxException, IOException, ExecutionException, InterruptedException {
@@ -776,19 +558,24 @@ public class 온프레미스_지라이슈_전략 implements 지라이슈_전략 
         // 이슈 건수가 1000이 넘을때 이슈 조회를 위한 처리
         List<지라이슈_데이터> 반환할_서브테스크_목록 = new ArrayList<>();
         SearchResult searchResult;
+        try {
+            do {
+                searchResult = restClient.getSearchClient()
+                        .searchJql(jql, 최대_검색수, startAt, fields)
+                        .get();
+                for (Issue issue : searchResult.getIssues()) {
+                    지라이슈_데이터 지라이슈_데이터 = 지라이슈_데이터로_변환(issue);
+                    반환할_서브테스크_목록.add(지라이슈_데이터);
+                }
 
-        do {
-            searchResult = restClient.getSearchClient()
-                    .searchJql(jql, 최대_검색수, startAt, fields)
-                    .get();
-            for (Issue issue : searchResult.getIssues()) {
-                지라이슈_데이터 지라이슈_데이터 = 지라이슈_데이터로_변환(issue);
-                반환할_서브테스크_목록.add(지라이슈_데이터);
+                startAt += 최대_검색수;
+            } while (searchResult.getTotal() > startAt);
+
+            return 반환할_서브테스크_목록;
+        }
+        catch (Exception e) {
+                로그.error("온프레미스에서 조회하려는 이슈 키의 서브테스크 정보 가져오기에 실패하였습니다. 조회 대상 정보 확인이 필요합니다.");
+                return null;
             }
-
-            startAt += 최대_검색수;
-        } while (searchResult.getTotal() > startAt);
-
-        return 반환할_서브테스크_목록;
-    }
+        }
 }
